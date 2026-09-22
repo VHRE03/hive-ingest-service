97f71e1bad3777b5456a41daa0b2b935e68918fc
VHRE03 <vh.romero.e@gmail.com>
2026-09-22 14:37:08 -0600

    first commit

---
+++ src/test/java/com/vhre/ingest/HiveIngestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhre.ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveIngestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
